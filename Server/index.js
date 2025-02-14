80ef8d0f7dcba32845098f62e0876379b0c3abdc
Rahulkrsharma2004 <kumarrahulbasdiha@gmail.com>
2025-02-14 15:55:37 +0530

    cors config and url set

--- Server/index.js
+++ Server/index.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: "https://video-calling-app-rk.vercel.app",
     methods: ["GET", "POST"],
   },
 });
