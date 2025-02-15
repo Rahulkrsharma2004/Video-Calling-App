06ad1a37c895b2f9dbaa3423cc29fe654ee54d1f
Rahulkrsharma2004 <kumarrahulbasdiha@gmail.com>
2025-02-15 17:08:10 +0530

    done

--- Client/vite.config.js
+++ Client/vite.config.js
@@ -1,27 +1,9 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import { NodeGlobalsPolyfillPlugin } from "@esbuild-plugins/node-globals-polyfill";
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      stream: "stream-browserify",
-      process: "process/browser",
-    },
-  },
-  optimizeDeps: {
-    esbuildOptions: {
-      define: {
-        global: "window",
-      },
-      plugins: [
-        NodeGlobalsPolyfillPlugin({
-          buffer: true,
-          process: true,
-        }),
-      ],
-    },
+  define: {
+    global: "window",
   },
 });
