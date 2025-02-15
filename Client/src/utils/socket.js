d1be75b66dd6366f68c79d1b5138fc101d13bf4b
Rahulkrsharma2004 <kumarrahulbasdiha@gmail.com>
2025-02-15 16:26:00 +0530

    resolve some error/

--- Client/src/utils/socket.js
+++ Client/src/utils/socket.js
@@ -1,5 +1,8 @@
 import { io } from "socket.io-client";
 
-const socket = io("https://video-calling-app-brown.vercel.app");
+const socket = io("https://videoc-app-backend.onrender.com", {
+  transports: ["websocket"],
+  withCredentials: true, 
+});
 
 export default socket;
